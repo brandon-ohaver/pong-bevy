145d6a06b9880f3d5468b7705c65d800df6ea511
brandon-ohaver <brandon.ohaver@gmail.com>
2024-09-10 11:00:49 -0500

    Updated positions for player/enemy/ball sprites

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,17 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn(SpriteBundle {
         texture: asset_server.load("player-sprite.png"),
+        transform: Transform::from_xyz(0.0, -200.0, 0.0),
+        ..default()
+    });
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("enemy-sprite.png"),
+        transform: Transform::from_xyz(0.0, 200.0, 0.0),
+        ..default()
+    });
+    commands.spawn(SpriteBundle {
+        texture:asset_server.load("ball-sprite.png"),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
         ..default()
     });
 }
