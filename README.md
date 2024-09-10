4fe101aa8763b0fb32fc6ae5e6669bccbe4cd946
brandon-ohaver <brandon.ohaver@gmail.com>
2024-09-10 13:48:42 -0500

    Added player movement

--- README.md
+++ README.md
@@ -7,12 +7,12 @@
 * Need to make bevy dependency DONE
 * Need to create player/opponent sprite DONE
 * Need to create ball sprite DONE
-* Need to setup starting position of player/enemy/ball
-* Need to make controls for player
+* Need to setup starting position of player/enemy/ball DONE
+* Need to make controls for player DONE
 * Need to make ball move/learn game physics?
 * Need to create collision with player/opponent sprites
 * Need to make "walls" for ball to hit so it doesn't leave screen
-* Need to opponent movement/AI
+* Need to create opponent movement/AI
 * Need to add scoring/point tracking
 * Need to add spawn/despawn logic for the ball when it is scored
 * Need to add sound
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Player;
+
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
     .add_systems(Startup, setup)
+    .add_systems(Update, player_movement)
     .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
+    commands.spawn((Player, SpriteBundle {
         texture: asset_server.load("player-sprite.png"),
         transform: Transform::from_xyz(0.0, -200.0, 0.0),
         ..default()
-    });
+    }));
     commands.spawn(SpriteBundle {
         texture: asset_server.load("enemy-sprite.png"),
         transform: Transform::from_xyz(0.0, 200.0, 0.0),
@@ -24,4 +28,16 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         transform: Transform::from_xyz(0.0, 0.0, 0.0),
         ..default()
     });
+}
+
+fn player_movement(input: Res<ButtonInput<KeyCode>>, mut player: Query<(&mut Transform, &mut Player)>) {
+    for (mut transform, mut _p) in &mut player {
+        let direction = transform.local_x();
+        if input.pressed(KeyCode::KeyA) {
+            transform.translation += direction * -1.0;
+        }
+        if input.pressed(KeyCode::KeyD) {
+            transform.translation += direction * 1.0;
+        }
+    }
 }
